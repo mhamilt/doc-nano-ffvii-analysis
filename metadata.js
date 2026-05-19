7d5e8e6f00be06cf564d62575c25d93925584d76
Matthew Hamilton <matthew.hamilton2@unibo.it>
2026-05-19 21:04:39 +0100

    add nodes and arrows

---
+++ metadata.js
@@ -0,0 +1,444 @@
+export const nodes = [
+  {
+    connections: ['prelude_1','prelude_2'],
+    text: 'The Prelude',
+    arrows_from: 1,
+    x: 294,
+    y: 205,
+    id: 'the-prelude'
+  },
+  {
+    connections: ['prelude_1'], text: 'Continue?', arrows_from: 1, x: 846, y: 146, id: 'continue'
+  },
+  {
+    connections: ['prelude_1'],
+    text: 'Chasing the\nBlack-Caped\nMan',
+    arrows_from: 1,
+    x: 919,
+    y: 287,
+    id: 'chasing-the-black-caped-man'
+  },
+  {
+    connections: ['prelude_1','prelude_2'],
+    text: 'Anxious\nHeart',
+    arrows_from: 1,
+    x: 792,
+    y: 417,
+    id: 'anxious-heart'
+  },
+  {
+    connections: ['prelude_2'],
+    text: 'Those Chosen\nBy the Planet',
+    arrows_from: 1,
+    x: 150,
+    y: 599,
+    id: 'those-chosen-by-the-planet'
+  },
+  {
+    connections: ['prelude_2','prelude_3'],
+    text: 'Weapon Raid',
+    arrows_from: 1,
+    x: 334,
+    y: 606,
+    id: 'weapon-raid'
+  },
+  {
+    connections: ['prelude_2','prelude_3'],
+    text: 'Birth of a God',
+    arrows_from: 1,
+    x: 518,
+    y: 613,
+    id: 'birth-of-a-god'
+  },
+  {
+    connections: ['prelude_2'],
+    text: 'One-Winged\nAngel',
+    arrows_from: 1,
+    x: 714,
+    y: 601,
+    id: 'one-winged-angel'
+  },
+  {
+    connections: ['chocobo_1'],
+    text: 'Waltz de\nChocobo',
+    arrows_from: 1,
+    x: 2257,
+    y: 306,
+    id: 'waltz-de-chocobo'
+  },
+  {
+    connections: ['fighting_2','fighting_3'],
+    text: 'Those Who\nFight Further',
+    arrows_from: 1,
+    x: 2712,
+    y: 1047,
+    id: 'those-who-fight-further'
+  },
+  {
+    connections: ['barret_1'],
+    text: 'Barret’s\nTheme',
+    arrows_from: 1,
+    x: 1219,
+    y: 209,
+    id: 'barrets-theme'
+  },
+  {
+    connections: ['fighting_2'],
+    text: 'Lurking in the\nDarkness',
+    arrows_from: 1,
+    x: 2523,
+    y: 1047,
+    id: 'lurking-in-the-darkness'
+  },
+  {
+    connections: ['fighting_2'],
+    text: 'Turks’ Theme',
+    arrows_from: 1,
+    x: 2775,
+    y: 860,
+    id: 'turks-theme'
+  },
+  {
+    connections: ['fighting_3'],
+    text: 'The Mako\nCannon Is\nFired',
+    arrows_from: 1,
+    x: 3301,
+    y: 860,
+    id: 'the-mako-cannon-is-fired'
+  },
+  {
+    connections: ['fighting_3','fighting_4'],
+    text: 'Shinra\nCompany',
+    arrows_from: 2,
+    x: 3207,
+    y: 990,
+    id: 'shinra-company'
+  },
+  {
+    connections: ['fighting_3','fighting_4'],
+    text: 'Infiltrating\nShinra\nTower',
+    arrows_from: 1,
+    x: 3066,
+    y: 1049,
+    id: 'infiltrating-shinra-tower'
+  },
+  {
+    connections: ['aerith_1'],
+    text: 'Judgment Day',
+    arrows_from: 1,
+    x: 150,
+    y: 1250,
+    id: 'judgment-day'
+  },
+  {
+    connections: ['chocobo_1'],
+    text: 'Cinco de\nChocobo',
+    arrows_from: 1,
+    x: 2245,
+    y: 132,
+    id: 'cinco-de-chocobo'
+  },
+  {
+    connections: ['barret_1'],
+    text: 'Mark of\nthe Traitor',
+    arrows_from: 1,
+    x: 1216,
+    y: 575,
+    id: 'mark-of-the-traitor'
+  },
+  {
+    connections: ['main_theme_1'],
+    text: 'Main Theme',
+    arrows_from: 1,
+    x: 1620,
+    y: 198,
+    id: 'main-theme'
+  },
+  {
+    connections: ['main_theme_1'],
+    text: 'Highwind Takes\nto the Skies',
+    arrows_from: 1,
+    x: 1839,
+    y: 188,
+    id: 'highwind-takes-to-the-skies'
+  },
+  {
+    connections: ['main_theme_1'],
+    text: 'Holding My\nThoughts in\nMy Heart',
+    arrows_from: 1,
+    x: 2011,
+    y: 313,
+    id: 'holding-my-thoughts-in-my-heart'
+  },
+  {
+    connections: ['main_theme_1'],
+    text: 'Interrupted by\nFireworks',
+    arrows_from: 1,
+    x: 2028,
+    y: 589,
+    id: 'interrupted-by-fireworks'
+  },
+  {
+    connections: ['main_theme_1'],
+    text: 'Steal the Tiny\nBronco!',
+    arrows_from: 1,
+    x: 1627,
+    y: 608,
+    id: 'steal-the-tiny-bronco'
+  },
+  {
+    connections: ['main_theme_1'],
+    text: 'That Day\nFive Years Ago',
+    arrows_from: 1,
+    x: 1478,
+    y: 500,
+    id: 'that-day-five-years-ago'
+  },
+  {
+    connections: ['main_theme_1'],
+    text: 'Who\nAm I?',
+    arrows_from: 1,
+    x: 1481,
+    y: 327,
+    id: 'who-am-i'
+  },
+  {
+    connections: ['fighting_4'],
+    text: 'Mako Reactor',
+    arrows_from: 1,
+    x: 3004,
+    y: 1191,
+    id: 'mako-reactor'
+  },
+  {
+    connections: ['chocobo_1'], text: 'Farm Boy', arrows_from: 0, x: 2792, y: 129, id: 'farm-boy'
+  },
+  {
+    connections: ['bombing_mission_1'],
+    text: 'Bombing\nMission',
+    arrows_from: 1,
+    x: 2028,
+    y: 1542,
+    id: 'bombing-mission'
+  },
+  {
+    connections: ['chocobo_1'],
+    text: 'Fiddle de\nChocobo',
+    arrows_from: 1,
+    x: 2457,
+    y: 318,
+    id: 'fiddle-de-chocobo'
+  },
+  {
+    connections: ['chocobo_1'],
+    text: 'Electric de\nChocobo',
+    arrows_from: 1,
+    x: 2632,
+    y: 325,
+    id: 'electric-de-chocobo'
+  },
+  {
+    connections: ['yuffie_1'],
+    text: 'Descendant of\nShinobi',
+    arrows_from: 1,
+    x: 186,
+    y: 1021,
+    id: 'descendant-of-shinobi'
+  },
+  {
+    connections: ['yuffie_1'],
+    text: 'Stolen\nMateria',
+    arrows_from: 1,
+    x: 818,
+    y: 1023,
+    id: 'stolen-materia'
+  },
+  {
+    connections: ['tifa_1'],
+    text: 'Tifa’s Theme',
+    arrows_from: 1,
+    x: 1070,
+    y: 849,
+    id: 'tifas-theme'
+  },
+  {
+    connections: ['fighting_3'],
+    text: 'Shinra Army\nWages a Full-\nScale Attack',
+    arrows_from: 1,
+    x: 2877,
+    y: 1073,
+    id: 'shinra-army-wages-a-full--scale-attack'
+  },
+  {
+    connections: ['aerith_1','aerith_2'],
+    text: 'Aerith’s\nTheme',
+    arrows_from: 2,
+    x: 483,
+    y: 1250,
+    id: 'aeriths-theme'
+  },
+  {
+    connections: ['aerith_1','aerith_2'],
+    text: 'Flowers\nBlooming in\nthe Church',
+    arrows_from: 1,
+    x: 632,
+    y: 1405,
+    id: 'flowers-blooming-in-the-church'
+  },
+  {
+    connections: ['aerith_2'], text: 'Reunion', arrows_from: 1, x: 820, y: 1396, id: 'reunion'
+  },
+  {
+    connections: ['cid_1'],
+    text: 'Cid’s Theme',
+    arrows_from: 1,
+    x: 903,
+    y: 1709,
+    id: 'cids-theme'
+  },
+  {
+    connections: ['cid_1'],
+    text: 'The Countdown\nBegins',
+    arrows_from: 1,
+    x: 1087,
+    y: 1759,
+    id: 'the-countdown-begins'
+  },
+  {
+    connections: ['cid_1'],
+    text: 'Sending a\nDream Into\nthe Universe',
+    arrows_from: 1,
+    x: 1271,
+    y: 1738,
+    id: 'sending-a-dream-into-the-universe'
+  },
+  {
+    connections: ['red_xii_1','red_xii_2'],
+    text: 'Cosmo\nCanyon',
+    arrows_from: 2,
+    x: 1632,
+    y: 1367,
+    id: 'cosmo-canyon'
+  },
+  {
+    connections: ['red_xii_1'],
+    text: 'Great Warrior',
+    arrows_from: 1,
+    x: 1530,
+    y: 1683,
+    id: 'great-warrior'
+  },
+  {
+    connections: ['red_xii_2'],
+    text: 'Red XIII’s\nTheme',
+    arrows_from: 1,
+    x: 1731,
+    y: 1686,
+    id: 'red-xiiis-theme'
+  },
+  {
+    connections: ['lifestream_1','lifestream_2'], text: 'Opening', arrows_from: 2, x: 2004, y: 1056, id: 'opening'
+  },
+  {
+    connections: ['lifestream_1','lifestream_2'],
+    text: 'Lifestream',
+    arrows_from: 1,
+    x: 2002,
+    y: 1313,
+    id: 'lifestream'
+  },
+  {
+    connections: ['fanfare_1'],
+    text: 'A Great\nSuccess',
+    arrows_from: 1,
+    x: 2794,
+    y: 1415,
+    id: 'a-great-success'
+  },
+  {
+    connections: ['bombing_mission_1'], text: 'Hurry!', arrows_from: 1, x: 2566, y: 1542, id: 'hurry'
+  },
+  {
+    connections: ['fighting_1','fighting_3'],
+    text: 'Hurry Faster!',
+    arrows_from: 1,
+    x: 3183,
+    y: 724,
+    id: 'hurry-faster'
+  },
+  {
+    connections: ['fanfare_1'], text: 'Fanfare', arrows_from: 1, x: 3075, y: 1606, id: 'fanfare'
+  },
+  {
+    connections: ['fanfare_1'],
+    text: 'Tango of\nTears',
+    arrows_from: 1,
+    x: 3301,
+    y: 1415,
+    id: 'tango-of-tears'
+  },
+  {
+    connections: ['oppressed_people_1'],
+    text: 'Oppressed\nPeople',
+    arrows_from: 1,
+    x: 2049,
+    y: 1806,
+    id: 'oppressed-people'
+  },
+  {
+    connections: ['oppressed_people_1','oppressed_people_2'],
+    text: 'It’s Hard to\nStand on Two\nFeet, Isn’t It?',
+    arrows_from: 1,
+    x: 2500,
+    y: 1806,
+    id: 'its-hard-to-stand-on-two-feet,-isnt-it'
+  },
+  {
+    connections: ['oppressed_people_2'],
+    text: 'If You Open\nYour Heart…',
+    arrows_from: 1,
+    x: 3037,
+    y: 1806,
+    id: 'if-you-open-your-heart'
+  },
+  {
+    connections: ['main_theme_1','fighting_1','fighting_2'],
+    text: 'Under the\nRotting Pizza',
+    arrows_from: 1,
+    x: 2716,
+    y: 679,
+    id: 'under-the-rotting-pizza'
+  },
+  {
+    connections: ['fighting_1','fighting_3'],
+    text: 'Those Who\nFight',
+    arrows_from: 1,
+    x: 2980,
+    y: 704,
+    id: 'those-who-fight'
+  },
+  {
+    connections: ['fighting_1'],
+    text: 'Trail of\nBlood',
+    arrows_from: 0,
+    x: 3301,
+    y: 591,
+    id: 'trail-of-blood'
+  },
+  {
+    connections: ['main_theme_1','tifa_1','aerith_2','cid_1','lifestream_1'],
+    text: 'World Crisis',
+    arrows_from: 5,
+    x: 1801,
+    y: 900,
+    id: 'world-crisis'
+  },
+  {
+    connections: ['main_theme_1','lifestream_2'],
+    text: 'Staff Roll',
+    arrows_from: 1,
+    x: 2158,
+    y: 865,
+    id: 'staff-roll'
+  }
+]
